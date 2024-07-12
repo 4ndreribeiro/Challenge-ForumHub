88133d0dc9a2400893898d3b0acd35b08ea93fc9
4ndreribeiro <andreribeiroweb2009@gmail.com>
2024-07-12 17:20:29 -0300

    First commit

---
+++ src/main/java/br/com/crislei/forum_hub/dto/response/RepostaIdDto.java
@@ -0,0 +1,12 @@
+package br.com.crislei.forum_hub.dto.response;
+
+import java.time.LocalDateTime;
+
+public record RepostaIdDto(
+                Long id,
+                String mensagem,
+                LocalDateTime data_criacao,
+                boolean solucao,
+                Long autorId,
+                Long topicId) {
+}
